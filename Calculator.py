ebbb6000922ebc5fbfa32a551d80f5a6b0c7ec48
tejupatil18 <patilsonu0118@gmail.com>
2026-08-19 17:37:30 +0530

    Add Simple Calculator project

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+
+Addition = a + b
+Subtraction = a - b
+Multiplication = a * b
+Division = a / b
+
+
+print("Addition: ", Addition)
+print("Subtraction: ", Subtraction)
+print("Multiplication: ", Multiplication)
+print("Division: ", Division)
